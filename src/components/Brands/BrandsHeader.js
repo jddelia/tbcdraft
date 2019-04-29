cb9810ffa1d828fd3fe5e6159e90ceafe320d6a6
jddelia <jddelia123@gmail.com>
2019-04-29 16:28:44 -0400

    Make responsive

--- src/components/Brands/BrandsHeader.js
+++ src/components/Brands/BrandsHeader.js
@@ -5,6 +5,14 @@ const BrandsHeader = () => {
     <div className="header-title">
       <hr className="strikethrough" />
       <span className="header-title-text">OUR BRANDS</span>
+
+      <h2 className="mobile-brands-header">Welcome to TBC Corporation</h2>
+      <span className="mobile-brands-subheader">
+        A leader in the tire and automotive 
+        service industry with a diverse corporate 
+        portfolio of several trusted,
+         well-known brands.
+      </span>
     </div>
   );
 };
